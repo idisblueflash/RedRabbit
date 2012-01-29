d32b7d5993a4196faea27fc72cca01c9965266ca
Hu Songtao <idisblueflash@gmail.com>
2012-01-29 09:29:26 +0800

    search-resource-page: ajax return results in JQM

--- red-cgi/search-resource.pl
+++ red-cgi/search-resource.pl
@@ -6,8 +6,10 @@ use CGI;
 my $q = new CGI;
 my $name = $q->param("search-key-editor");
 
+my @outputText;
 my @pictures = ("apple", "banana", "pineapple", "apple pie");
 my @found_pictures = grep(/$name/, @pictures);
+
 #fullPageTest();
 partPageTest();
 
@@ -15,11 +17,15 @@ sub partPageTest{
   print $q->header;
     if (@found_pictures){
       foreach(@found_pictures){
-        print $q->p("found: $_!");
+        # print $q->p("found: $_!");
+        push @outputText, "found: $_ .";
       }
     } else {
-        print $q->p("Nothing found.");
+        # print $q->p("Nothing found.");
+        push @outputText, "Nothing found.";
     }
+    my $return = join(" ", @outputText);
+    print $return;
 }       
 
 
