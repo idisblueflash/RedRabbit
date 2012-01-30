6f0e53b99cee10ba4991f615c041b6c92c583f67
Hu Songtao <idisblueflash@gmail.com>
2012-01-30 08:43:49 +0800

    search-resource-page: cgi return json format

--- red-cgi/search-resource.pl
+++ red-cgi/search-resource.pl
@@ -9,21 +9,57 @@ my $name = $q->param("search-key-editor");
 my @outputText;
 
 #fullPageTest();
-&searchPictures($name);
-&searchVideos($name);
+&searchPicturesJson($name);
+#&searchPictures($name);
+#&searchVideos($name);
 
 #out put the results
 print $q->header;
-print &ulPackage();
+print &ulPackageJson();
+#print &ulPackage();
 
 # functions 
+sub generate_random_string {
+    my $res = '';
+    $res.= chr int rand(26)+96 for 1..8;
+    return $res;
+}
+
+sub searchPicturesJson{
+  my $key = shift;
+  my @pictures = ("apple", "banana", "pineapple", "apple pie");
+  my @found_pictures = grep(/$key/, @pictures);
+
+    if (@found_pictures){
+
+      foreach(@found_pictures){
+        my $id = generate_random_string();
+        # print $q->p("found: $_!");
+        my $image_caption = $_; 
+        my $image_filename = $_ ;
+        $image_filename =~ s/\ /_/g;
+        push @outputText, "\t{id: id, describe: '$_', type: 'pictuers', filename: '../images/thumbnails/$image_filename'}";
+      }
+    } else {
+      # nothing found.
+    }
+}       
+
+sub ulPackageJson(){
+    my $return = join(", \n", @outputText);
+    $return = "[ \n"
+    . $return 
+    . "\n ]";
+    return $return;
+}
+
 sub searchVideos{
   my $key = shift;
   my @videos= ("bad apple black and white", "apple introducing ipad 2");
   my @found_videos= grep(/$key/, @videos);
 
+    push @outputText, "\t<li data-role=list-divider>Videos:</li>";
     if (@found_videos){
-      push @outputText, "\t<li data-role=list-divider>Videos:</li>";
 
       foreach(@found_videos){
         # print $q->p("found: $_!");
@@ -51,8 +87,8 @@ sub searchPictures{
   my @pictures = ("apple", "banana", "pineapple", "apple pie");
   my @found_pictures = grep(/$key/, @pictures);
 
+    push @outputText, "\t<li data-role=list-divider>Pictures:</li>";
     if (@found_pictures){
-      push @outputText, "\t<li data-role=list-divider>Pictures:</li>";
 
       foreach(@found_pictures){
         # print $q->p("found: $_!");
